5664b1eec268547d8a4ec1e4f8bc19755c76adef
EsunR <641411169@qq.com>
2019-12-06 01:22:28 +0800

    update

--- client/src/router.js
+++ client/src/router.js
@@ -1,23 +1,8 @@
 import Vue from "vue"
 import Router from "vue-router"
-import home from "@/views/home"
-import login from "@/views/login"
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: "/",
-      redirect: "/home"
-    },
-    {
-      path: "/login",
-      component: login
-    },
-    {
-      path: "/home",
-      component: home
-    }
-  ]
+  routes: []
 })
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,21 +1,13 @@
 module.exports = {
-  "configureWebpack": {
-    "resolve": {
-      "alias": {
+  configureWebpack: {
+    resolve: {
+      alias: {
         "@": "D:\\Code\\GitProject\\Qiniu-Serve\\client\\src",
-        "components": "D:\\Code\\GitProject\\Qiniu-Serve\\client\\src\\components",
-        "views": "D:\\Code\\GitProject\\Qiniu-Serve\\client\\src\\views"
+        components:
+          "D:\\Code\\GitProject\\Qiniu-Serve\\client\\src\\components",
+        views: "D:\\Code\\GitProject\\Qiniu-Serve\\client\\src\\views"
       }
     }
   },
-  "css": {
-    "loaderOptions": {
-      "sass": {
-        "prependData": "@import \"~@/style/variable.scss\";"
-      }
-    }
-  },
-  "transpileDependencies": [
-    "vuetify"
-  ]
+  transpileDependencies: ["vuetify"]
 }
